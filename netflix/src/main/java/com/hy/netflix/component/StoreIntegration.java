bb41da72de6c47807b903f441bfd47445c2cb47f
Alex-innover <n@hotmail.com>
2019-06-14 14:02:48 +0800

    init

---
+++ netflix/src/main/java/com/hy/netflix/component/StoreIntegration.java
@@ -0,0 +1,14 @@
+package com.hy.netflix.component;
+
+import java.util.Map;
+
+public class StoreIntegration {
+	
+	/*
+	 * @HystrixCommand(commandProperties = {
+	 * 
+	 * @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",
+	 * value = "500") }) public User getUserById(String id) { return
+	 * userResource.getUserById(id); }
+	 */
+}
